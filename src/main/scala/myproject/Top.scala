d0bed5cd2684e8670af9fb7a5c6501d5a1062a95
Shaurya Pratap Singh <shauryadevendrasingh@gmail.com>
2026-06-28 12:43:52 +0530

    Update Top.scala

--- src/main/scala/myproject/Top.scala
+++ src/main/scala/myproject/Top.scala
@@ -16,6 +16,12 @@ object gcd8 extends App with emitrtl.Toplevel {
 
 }
 
+object fullAdder extends App with emitrtl.Toplevel {
+  lazy val topModule = new FullAdder
+  chisel2firrtl()
+  firrtl2sv()
+}
+
 /** To run from a terminal shell
   * {{{
   * ./mill gcd.runMain gcd.gcd16
