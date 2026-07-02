65c9ad7dcc40ff5365028d4848f8bb7ab7694771
Shaurya Pratap Singh <shauryadevendrasingh@gmail.com>
2026-07-02 11:37:49 +0530

    Update tester3.scala

--- src/main/scala/myproject/tester3.scala
+++ src/main/scala/myproject/tester3.scala
@@ -1,12 +1,12 @@
 import chisel3._
 import chisel3.util._
 
-class ALU extends Module {
+clas ALU extends Module {
   val io = IO(new Bundle {
     val fn     = Input(UInt(2.W)) // Operation select: 0=Add, 1=Sub, 2=OR, 3=AND
     val a      = Input(UInt(4.W)) // 4-bit input A
     val b      = Input(UInt(4.W)) // 4-bit input B
-    val result = Output(UInt()) // 4-bit output
+    val result = Output(UInt(4.W)) // 4-bit output
   })
 
   // Default output value to prevent inferred latches
