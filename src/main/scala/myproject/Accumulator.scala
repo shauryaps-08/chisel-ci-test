5e5ac06f612dad821f91094975917b24c37a1fdb
shauryaps_08 <shauryapratapsingh060208@gmail.com>
2026-07-02 08:39:56 +0000

    Add Accumulator module

---
+++ src/main/scala/myproject/Accumulator.scala
@@ -0,0 +1,27 @@
+package myproject
+import chisel3._
+
+// A parameterized Accumulator module
+class Accumulator(val bitWidth: Int = 4) extends Module {
+  val io = IO(new Bundle {
+    val en    = Input(Bool())
+    val dataIn = Input(UInt(bitWidth.W))
+    val out    = Output(UInt(bitWidth.W))
+  })
+
+  // Register initialized to 0 on reset
+  val countReg = RegInit(0.U(bitWidth.W))
+
+  // Conditional combinational logic
+  when(io.en) {
+    countReg := countReg + io.dataIn
+  }
+
+  // Assign register output to the I/O port
+  io.out := countReg
+}
+
+// Object to generate the SystemVerilog output
+object AccumulatorMain extends App {
+  emitVerilog(new Accumulator(4))
+}
