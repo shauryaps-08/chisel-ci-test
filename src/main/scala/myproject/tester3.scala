35979f5312d99424bef79f57f108ff1844717682
Shaurya Pratap Singh <shauryadevendrasingh@gmail.com>
2026-07-02 11:33:24 +0530

    Update tester3.scala

--- src/main/scala/myproject/tester3.scala
+++ src/main/scala/myproject/tester3.scala
@@ -6,7 +6,7 @@ class ALU extends Module {
     val fn     = Input(UInt(2.W)) // Operation select: 0=Add, 1=Sub, 2=OR, 3=AND
     val a      = Input(UInt(4.W)) // 4-bit input A
     val b      = Input(UInt(4.W)) // 4-bit input B
-    val result = Output(UInt(4.W)) // 4-bit output
+    val result = Output(UInt()) // 4-bit output
   })
 
   // Default output value to prevent inferred latches
