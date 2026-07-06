187cbf3f1fc57d387b54f7d04a63b18f8b6c7bb1
Shaurya Pratap Singh <shauryadevendrasingh@gmail.com>
2026-07-06 10:25:19 +0530

    Create tester5.scala

---
+++ src/main/scala/myproject/tester5.scala
@@ -0,0 +1,15 @@
+package myproject
+
+class MyModule(width: Int) extends Module {
+  val io = IO(new Bundle {
+    val in  = Input(UInt(3.W))
+    val out = Output(UInt(8.W))
+  })
+  io.out := (1.U << io.in)
+}
+
+object MyModuleMain extends App with emitrtl.Toplevel {
+  lazy val topModule = new MyModule(8)
+  chisel2firrtl()
+  firrtl2sv()
+}
